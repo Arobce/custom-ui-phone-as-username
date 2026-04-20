5fa53911afbb311af6f9a474ff08c68a4030d601
Roshan Chapagain <arobce@gmail.com>
2026-04-20 14:20:16 -0500

    move to default page

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { type KindePageEvent } from "@kinde/infrastructure";
+import { getKindeNonce } from "@kinde/infrastructure";
 import React from "react";
 import { renderToString } from "react-dom/server.browser";
 import { Widget } from "../../../../components/widget";
@@ -8,11 +9,43 @@ import { DefaultLayout } from "../../../../layouts/default";
 import { Root } from "../../../../root";
 
 const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
+  const nonce = getKindeNonce();
+
   return (
     <Root context={context} request={request}>
       <DefaultLayout>
         <Widget heading={context.widget.content.heading} />
       </DefaultLayout>
+      <script
+        nonce={nonce}
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function() {
+              console.log("Base inline script executed on default page");
+
+              async function runExampleApiCall() {
+                try {
+                  var response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+                  if (!response.ok) {
+                    throw new Error("Example API call failed with status " + response.status);
+                  }
+
+                  var result = await response.json();
+                  console.log("Example API response:", result);
+                } catch (error) {
+                  console.error("Example API error:", error);
+                }
+              }
+
+              if (document.readyState === "complete") {
+                runExampleApiCall();
+              } else {
+                window.addEventListener("load", runExampleApiCall, { once: true });
+              }
+            })();
+          `,
+        }}
+      />
     </Root>
   );
 };
